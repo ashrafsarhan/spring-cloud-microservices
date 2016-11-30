7a8b0cd8d3d01ddd56f185244dcffc81eb2f4a85
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-11-30 20:58:53 +0100

    first commit

---
+++ eureka-server/src/main/java/com/axiell/server/eureka/EurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.axiell.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
