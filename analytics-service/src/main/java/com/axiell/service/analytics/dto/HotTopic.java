568b74e11aeb74d095c2f76091c9c5981b03859f
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-12-03 19:24:50 +0100

    Adding HotTopic DTO

---
+++ analytics-service/src/main/java/com/axiell/service/analytics/dto/HotTopic.java
@@ -0,0 +1,20 @@
+package com.axiell.service.analytics.dto;
+
+/**
+ * @author ashraf
+ *
+ */
+public class HotTopic {
+
+	private String value;
+	private Long valueCount;
+
+	public String getValue() {
+		return value;
+	}
+
+	public Long getValueCount() {
+		return valueCount;
+	}
+
+}
