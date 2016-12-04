03eea4b8c44f8b087814cf0084d3105b2487d07e
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-12-04 19:51:49 +0100

    Enable Eureka service discovery

--- dashboard-app/src/main/java/com/axiell/app/dashboard/DashboardAppApplication.java
+++ dashboard-app/src/main/java/com/axiell/app/dashboard/DashboardAppApplication.java
@@ -1,9 +1,15 @@
 package com.axiell.app.dashboard;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Import;
 
+@EnableAutoConfiguration
+@EnableEurekaClient
 @SpringBootApplication
+@Import(RestConfig.class)
 public class DashboardAppApplication {
 
 	public static void main(String[] args) {
